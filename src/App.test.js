0f01a6618581bb356082ab683c5dc2a072aec1b1
Dylan Bedetti <dylanbedetti@gmail.com>
2020-12-28 08:55:44 +0000

    added button tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,11 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent, cleanup } from '@testing-library/react'
 
 import App from './App'
+
+// what do I actually care about when testing?
+// press different buttons and different things get displayed?
+// make CRUD actions and test if they happen
 
 describe('true is truthy and false is falsy', () => {
   test('true is truthy', () => {
@@ -16,4 +20,20 @@ describe('true is truthy and false is falsy', () => {
     const { getByText } = render(<App />)
     expect(getByText('Organise My Life')).toBeInTheDocument()
   })
+
+  test("Loads TO DO's after button click", () => {
+    const { getByText } = render(<App />)
+    const button = getByText("TO DO's")
+    fireEvent.click(button)
+    expect(getByText('Add task')).toBeInTheDocument()
+  })
+
+  test('Loads Completed after button click', () => {
+    const { getByText } = render(<App />)
+    const button = getByText('Completed')
+    fireEvent.click(button)
+    expect(getByText('Add task')).toBeInTheDocument()
+  })
+
+  afterEach(cleanup)
 })
